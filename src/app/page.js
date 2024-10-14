6bb0321e2be927913c0eff5132c0ccb3f2aff309
kunal89204 <kunalkhandelwal108@gmail.com>
2024-10-14 19:27:33 +0530

    faq added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+import Faq from '@/components/Faq'
 import Hero from '@/components/Hero'
 import React from 'react'
+
 
 const page = () => {
   return (
     <div className=''>
       <Hero/>
+      <Faq />
     </div>
   )
 }
