b3f0693234d2a8b5dbc15d5dbd9abffbb62baf6a
kunal89204 <kunalkhandelwal108@gmail.com>
2024-10-23 19:54:05 +0530

    nav bar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,64 @@ import React from "react";
 import logo from "@/assets/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <header className="flex items-center bg-transparent fixed w-full z-50 ">
+    <header className="flex items-center bg-transparent  w-full z-50 px-10">
       <div className="p-2">
         <Image src={logo} width={150} />
       </div>
       <nav className="flex gap-10 font-semibold w-full justify-center text-gray-700  items-center">
-        <Link className="opacity-80 hover:opacity-100 transition-all duration-100-" href={"/"}>
+        <Link
+          className="opacity-80 hover:opacity-100 transition-all duration-100-"
+          href={"/"}
+        >
           Home
         </Link>
-        <Link className="opacity-80 hover:opacity-100 transition-all duration-100-" href={"/"}>
+        <Link
+          className="opacity-80 hover:opacity-100 transition-all duration-100-"
+          href={"/"}
+        >
+          PPF
+        </Link>
+        <Link
+          className="opacity-80 hover:opacity-100 transition-all duration-100-"
+          href={"/"}
+        >
           Products
         </Link>
-        <Link className="opacity-80 hover:opacity-100 transition-all duration-100-" href={"/"}>
-          Gallery
+        <Link
+          className="opacity-80 hover:opacity-100 transition-all duration-100-"
+          href={"/"}
+        >
+          About Us
         </Link>
-        <Link className="opacity-80 hover:opacity-100 transition-all duration-100-" href={"/"}>
-          Blogs
+        <Link
+          className="opacity-80 hover:opacity-100 transition-all duration-100-"
+          href={"/"}
+        >
+          Warranty
         </Link>
-        <Link className="opacity-80 hover:opacity-100 transition-all duration-100-" href={"/"}>
-          Contact Us
+        <Link
+          className="opacity-80 hover:opacity-100 transition-all duration-100-"
+          href={"/"}
+        >
+          Contact
         </Link>
-        <Link className="" href={"/"}>
-  <button className="bg-gradient-to-r transition-all from-[#86C5DA] to-[#66B2CA] border-2 text-xs font-bold text-white border-[#5EAFC5] py-3 px-8 rounded-lg shadow-md hover:from-[#66B2CA] hover:to-[#4FA1B8] hover:border-[#4FA1B8] focus:ring-4 focus:ring-[#66B2CA]/50 ease-in-out duration-300">
-    GET QUOTE
-  </button>
-</Link>
-
       </nav>
+      <div>
+        <Button
+          bg={"#FFBB4E"}
+          _focus={{ boxShadow: "none", bg: "#FFBB4E" }} // Disable default focus styling
+          rounded={"full"}
+          fontWeight={500}
+          fontSize={"14px"}
+          _hover={{ bg: "#FFBB4E" }}
+        >
+          Get a Quote
+        </Button>
+      </div>
     </header>
   );
 };
