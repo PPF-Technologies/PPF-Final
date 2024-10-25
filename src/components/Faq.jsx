3970b56e95ac21e5036c7e224fabfc4a75320335
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2024-10-25 18:04:54 +0530

    updated FAQ and Footer

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,13 +1,122 @@
-import { Text, Box } from '@chakra-ui/react'
-import React from 'react'
+"use client";
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  Flex,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Icon,
+} from "@chakra-ui/react";
+import { FiChevronRight, FiChevronDown } from "react-icons/fi";
+import AnimButton from "../components/props/Button";
+
+const FAQData = [
+  {
+    question: "What is your return policy?",
+    answer:
+      "We offer a hassle-free 30-day return policy. If you're unsatisfied with your purchase, return it within 30 days for a full refund.",
+  },
+  {
+    question: "Do you offer free shipping?",
+    answer:
+      "Yes, we offer free shipping on orders over $50. Standard shipping rates apply for orders under $50.",
+  },
+  {
+    question: "How can I track my order?",
+    answer:
+      "Once your order ships, you'll receive a tracking number via email. You can use this to track your package on our website.",
+  },
+  {
+    question: "Do you offer assembly services?",
+    answer:
+      "Yes, we offer professional assembly services for an additional fee. You can select this option during checkout.",
+  },
+];
+
+const FaqItem = ({ question, answer }) => (
+  <AccordionItem
+    border="none"
+    mb={2}
+    borderRadius="md"
+    maxW="100%"
+    mr={8}
+    background="linear-gradient(96.56deg, rgba(194, 218, 218, 0.3) 27.8%, rgba(240, 237, 226, 0.3) 53.91%)" 
+  >
+    {({ isExpanded }) => (
+      <>
+        <AccordionButton p={4} _hover={{ bg: "gray.100" }} borderRadius="md">
+          <Flex flex="1" justify="space-between" align="center">
+            <Text fontWeight="medium">{question}</Text>
+            <Text fontSize="2xl" ml={2}>
+              {isExpanded ? '×' : '+'}
+            </Text>
+          </Flex>
+        </AccordionButton>
+        <AccordionPanel
+          pb={4}
+          px={4}
+          transition="max-height 0.3s ease-in-out, opacity 0.3s ease-in-out"
+          maxHeight={isExpanded ? "200px" : "0px"} 
+          overflow="hidden" 
+        >
+          <Text color="#6C6C6C">{answer}</Text>
+        </AccordionPanel>
+      </>
+    )}
+  </AccordionItem>
+);
 
 
 const Faq = () => {
   return (
-    <Box>
-      <Text>Hello world</Text>
-    </Box>
-  )
-}
+    <Flex
+      direction="row"
+      align="top"
+      justify="space-between"
+      w="100%"
+      maxW="100vw"
+      mx={4}
+      py={8}
+      px={4}
+      
+    >
+      <Box textAlign="start" mb={8} w="35%" >
+        <Heading size="2xl" mb={4}>
+          Frequently Asked Questions
+        </Heading>
+        <AnimButton>Customer Support</AnimButton>
+      </Box>
 
-export default Faq
+      <Flex
+        wrap="wrap"
+        justify="flex-end"
+        alignItems="flex-end"
+        gap={4}
+        w="60%"
+      >
+        <Accordion allowToggle w="100%">
+          {FAQData.map((faq, index) => (
+            <FaqItem key={index} question={faq.question} answer={faq.answer} />
+          ))}
+        </Accordion>
+
+        <Button
+          variant="link"
+          colorScheme="blue"
+          alignSelf="flex-end"
+          rightIcon={<FiChevronRight />}
+          mt={4}
+          mr={6}
+        >
+          More Questions
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Faq;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,231 @@
-import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+"use client";
+
+import {
+  Box,
+  Container,
+  Stack,
+  SimpleGrid,
+  Text,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Icon,
+  Image,
+  HStack,
+  VStack,
+  Heading,
+  Flex,
+} from "@chakra-ui/react";
+import { BsArrowUpRight } from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
+import { IoLocationOutline } from "react-icons/io5";
+import { FiPhone } from "react-icons/fi";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <Box>
-        <Text as="p" fontSize="sm" color="gray.500" textAlign="center">
-          © 2022 PPF. All rights reserved.
-        </Text>
+    <Box
+      bg="linear-gradient(119.05deg, #282B33 20.98%, #4A7280 72.1%, #D2B6A0 99.13%)"
+      color="white"
+      py={16}
+    >
+      <Container maxW="100%" px={20}>
+        <Stack spacing={20}>
+          {/* Newsletter Section */}
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            justify="space-between"
+            align="center"
+          >
+            <Heading
+              as="h2"
+              color="#FFFFEE"
+              fontSize="6xl"
+              fontWeight="medium"
+              mb={{ base: 4, md: 0 }}
+              maxW={{ base: "100%", md: "60%" }}
+            >
+              Stay Connected <br />
+              and Inspired!
+            </Heading>
+            <Box
+              flex="1"
+              mt={{ base: 4, md: 0 }}
+              maxW={{ base: "100%", md: "40%" }}
+            >
+              <InputGroup size="lg" bg="transparent">
+                <Input
+                  placeholder="Enter Your Email"
+                  fontSize="2xl"
+                  _placeholder={{ color: "white", fontSize: "4xl" }}
+                  borderColor="white"
+                  borderWidth={0}
+                  borderBottomWidth={1}
+                  borderRadius={0}
+                  pl={0}
+                  _focus={{
+                    borderColor: "white",
+                    boxShadow: "none",
+                  }}
+                />
 
+                <InputRightElement>
+                  <Icon as={BsArrowUpRight} boxSize={6} color="#FFBB4E" />
+                </InputRightElement>
+              </InputGroup>
+            </Box>
+          </Flex>
+
+          {/* Navigation and Contact Section */}
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8}>
+            {/* Social Media Links Section */}
+            <Box w={{ base: "100%", md: "40%" }}>
+              <VStack align="start">
+                <Text fontSize="lg" fontWeight="medium" mb={4}>
+                  Social Media
+                </Text>
+                <HStack spacing={6}>
+                  <Link
+                    href="#"
+                    display="flex"
+                    alignItems="center"
+                    aria-label="Instagram"
+                  >
+                    <Text mr={1} fontWeight="light">
+                      Instagram
+                    </Text>
+                    <Icon color="#FFBB4E" as={BsArrowUpRight} />
+                  </Link>
+                  <Link
+                    href="#"
+                    display="flex"
+                    alignItems="center"
+                    aria-label="Twitter"
+                  >
+                    <Text mr={1} fontWeight="light">
+                      Twitter
+                    </Text>
+                    <Icon color="#FFBB4E" as={BsArrowUpRight} />
+                  </Link>
+                  <Link
+                    href="#"
+                    display="flex"
+                    alignItems="center"
+                    aria-label="YouTube"
+                  >
+                    <Text mr={1} fontWeight="light">
+                      YouTube
+                    </Text>
+                    <Icon color="#FFBB4E" as={BsArrowUpRight} />
+                  </Link>
+                </HStack>
+              </VStack>
+            </Box>
+
+            {/* Links Section (Company, Help Center, Contact Information) */}
+            <Box w={{ base: "100%", md: "100%" }}>
+              <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
+                <Box>
+                  <Stack spacing={4}>
+                    <Text fontWeight="semibold" mb={2}>
+                      Company
+                    </Text>
+                    <Link href="#">About</Link>
+                    <Link href="#">Products</Link>
+                    <Link href="#">Installation Guide</Link>
+                    <Link href="#">Blog</Link>
+                    <Link href="#">Contact Us</Link>
+                    <Link href="#">FAQ</Link>
+                  </Stack>
+                </Box>
+                <Box>
+                  <Stack spacing={4}>
+                    <Text fontWeight="semibold" mb={2}>
+                      Help Center
+                    </Text>
+                    <Link href="#">Community</Link>
+                    <Link href="#">Knowledge Base</Link>
+                    <Link href="#">Academy</Link>
+                    <Link href="#">Support</Link>
+                  </Stack>
+                </Box>
+                <Box>
+                  <VStack align="start" spacing={4}>
+                    <HStack spacing={3} alignItems="flex-start">
+                      <Icon
+                        as={IoLocationOutline}
+                        border="2px"
+                        borderRadius="md"
+                        borderColor="#676767"
+                        color="#FFBB4E"
+                        boxSize={10}
+                        p={2}
+                        mt={1}
+                      />
+                      <Text>
+                        Ground Floor, Right Portion, KHASRA NO. 29/23, Theke
+                        Wali Gali, Near Sai Baba Mandir, Alipur, Delhi 110036
+                      </Text>
+                    </HStack>
+                    <HStack spacing={3}>
+                      <Icon
+                        as={FiPhone}
+                        border="2px"
+                        borderRadius="md"
+                        borderColor="#676767"
+                        color="#FFBB4E"
+                        boxSize={10}
+                        p={2}
+                      />
+                      <Text>9212302362 / 9315892606</Text>
+                    </HStack>
+                    <HStack spacing={3}>
+                      <Icon
+                        as={HiOutlineMail}
+                        border="2px"
+                        borderRadius="md"
+                        borderColor="#676767"
+                        color="#FFBB4E"
+                        boxSize={10}
+                        p={2}
+                      />
+                      <Text>contact@company.com</Text>
+                    </HStack>
+                  </VStack>
+                </Box>
+              </SimpleGrid>
+            </Box>
+          </SimpleGrid>
+
+          {/* Footer Bottom */}
+          <Stack
+            direction={{ base: "column", md: "row" }}
+            justify="space-between"
+            align="center"
+            pt={8}
+            borderTop="1px"
+            borderColor="whiteAlpha.300"
+          >
+            <Text>© {currentYear} Copyright By Camio PPF</Text>
+            <Image
+              src="/path-to-your-logo.png" // Replace with your logo path
+              alt="Camio Logo"
+              height="40px"
+            />
+            <HStack spacing={4}>
+              <Link href="#">Terms</Link>
+              <Link href="#">Privacy</Link>
+              <Link href="#">Cookies</Link>
+              <Link href="#">Legal</Link>
+              <Link href="#">Recalls</Link>
+            </HStack>
+          </Stack>
+        </Stack>
+      </Container>
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
